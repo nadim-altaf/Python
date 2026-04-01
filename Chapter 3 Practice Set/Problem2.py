bc1886a516d7b244984c5aa5a104419a5d51036e
nadim-altaf <nadimaltaf271@gmail.com>
2026-04-01 15:46:34 +0530

    practice on string concept in python

---
+++ Chapter 3 Practice Set/Problem2.py
@@ -0,0 +1,5 @@
+letter = '''Dear <|Name|>,
+You are selected!
+<|Date|>'''
+
+print(letter.replace("<|Name|>","Nadim").replace("<|Date|>","02 March 2025"))
---
+++ Chapter 3 Practice Set/Problem3.py
@@ -0,0 +1,9 @@
+name = "Good Afternoon  Nadim"
+
+Find = name.find("  ") # --> gives index
+
+# print(Find)
+
+
+print(name.replace("  "," ")) # name wali string change nahi hovi hai ye ek naye string bane hai hai!
+print(name) # string are immutable which means that you cannot change them by runnung function on them
