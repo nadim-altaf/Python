567d6a47fefe8229b644d008b8004dc4de1b9617
nadim-altaf <nadimaltaf271@gmail.com>
2026-04-13 15:14:58 +0530

    Added class_method program

---
+++ Chapter 11 - inheritance/class_method.py
@@ -0,0 +1,20 @@
+class Student:
+    a = 1
+    @classmethod # decorator
+    def show(cls):
+        print(f"The class attribute is of (a) is {cls.a}")
+
+
+obj = Student()
+
+obj.a = 55 # not take this 
+obj.show()
+
+
+'''
+@classmethod is a decorator used to define a method that belongs to the class, not the instance.
+
+It takes cls (the class itself) as the first argument instead of self.
+
+This allows the method to access and modify class variables and call other class methods.
+'''
